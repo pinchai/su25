5cd74a13c52604a10df2cd3153a84bdba5882f74
pinchai <pinchai.pc@gmail.com>
2026-04-08 19:51:44 +0700

    get

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
+from product import product
 
 app = Flask(__name__)
 
 
 @app.get('/')
 def home():
-    return render_template('front/home.html')
+    return render_template('front/home.html', product=product)
 
 
 @app.get('/cart')
@@ -16,6 +17,17 @@ def cart():
 @app.get('/checkout')
 def checkout():
     return render_template('front/checkout.html')
+
+
+@app.get('/detail')
+def detail():
+    pro_id = request.args.get('pro_id')
+    title = request.args.get('title')
+    image = request.args.get('image')
+    price = request.args.get('price')
+    description = request.args.get('description')
+
+    return render_template('front/detail.html', pro_id=pro_id, title=title, image=image, price=price, description=description)
 
 
 @app.post('/create-user')
