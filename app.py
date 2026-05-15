0a2b971988584f59e2b03b83423af62c87d8e5e6
pinchai <pinchai.pc@gmail.com>
2026-05-15 20:38:00 +0700

    a

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from product import product
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydb.sqlite3"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False)
 @app.get('/')
 def home():
     return render_template('front/home.html', product=product)
